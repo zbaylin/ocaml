45e648373ad8fe246a57b97be04b23c73b79bc70
Yunxing Dai <yunxing@fb.com>
2016-11-01 14:17:50 -0700

    Make the error message more obvious

--- yarn-install-project.sh
+++ yarn-install-project.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
-set -e
-set -x
 # Check if space exists in path
 if [[ "$(pwd)" =~ ( |\') ]]
 then
+    echo "**************************************************************"
     echo "ERROR: refuse to work with a path containing space: \"$(pwd)\""
+    echo "**************************************************************"
     exit 1
 fi
+set -e
+set -x
 ./configure -no-cfi -prefix $(pwd)
 make -j world.opt
 make install
